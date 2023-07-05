15fb2b0b4ebcd957f573005804ac29f864f4581e
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2023-07-05 08:38:37 +0300

    Migrate cervical cancer

--- packages/esm-cervical-cancer-app/src/index.ts
+++ packages/esm-cervical-cancer-app/src/index.ts
@@ -1,5 +1,4 @@
-import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle, provide } from '@openmrs/esm-framework';
-import { backendDependencies } from './openmrs-backend-dependencies';
+import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle } from '@openmrs/esm-framework';
 import {
   caCxAppointmentsDashboardMeta,
   caCxSummaryDashboardMeta,
@@ -8,82 +7,46 @@ import {
 } from './dashboard.meta';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
-
-require('./root.scss');
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 export const moduleName = '@ohri/openmrs-esm-ohri-cervical-cancer-app';
 
-function setupOpenMRS() {
+require('./root.scss');
+
   const options = {
     featureName: 'ohri-cervical-cancer',
     moduleName,
   };
 
+export function startupApp() {
   defineConfigSchema(moduleName, {});
+ };
+export const cacxDashboard = getSyncLifecycle(createDashboardGroup(cervicalCancerFolderMeta),
+  options,
+ );
+export const cacxSummaryDashboardLink = getSyncLifecycle(createDashboardLink(caCxSummaryDashboardMeta),
+  options,
+ );
 
-  return {
-    pages: [],
-    extensions: [
-      {
-        id: 'cervical-cancer',
-        slot: 'patient-chart-dashboard-slot',
-        load: getSyncLifecycle(createDashboardGroup(cervicalCancerFolderMeta), options),
-        meta: cervicalCancerFolderMeta,
-        online: true,
-        offline: true,
-        order: 24,
-      },
-      {
-        id: 'cacx-summary-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxSummaryDashboardMeta), options),
-        meta: caCxSummaryDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-summary-ext',
-        slot: 'cacx-summary-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-summary/cacx-summary.component'), {
-          featureName: 'cacx-summary',
-          moduleName,
-        }),
-      },
-      {
-        id: 'cacx-visits-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxVisitsDashboardMeta), options),
-        meta: caCxVisitsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-visits-ext',
-        slot: 'cacx-visits-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-visits/cacx-visits-services.component'), {
-          featureName: 'cacx-visits',
-          moduleName,
-        }),
-      },
-      {
-        id: 'cacx-appointments-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxAppointmentsDashboardMeta), options),
-        meta: caCxAppointmentsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-appointments-ext',
-        slot: 'cacx-appointments-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-appointment/cacx-appointments.component'), {
-          featureName: 'cacx-appointments',
-          moduleName,
-        }),
-      },
-    ],
-  };
-}
+export const cacxSummaryDashboard = getAsyncLifecycle(() => import('./views/cacx-summary/cacx-summary.component'), {
+  featureName: 'cacx-summary',
+  moduleName,
+});
 
-export { backendDependencies, importTranslation, setupOpenMRS };
+export const cacxVisitsDashboardLink = getSyncLifecycle(  createDashboardLink(caCxVisitsDashboardMeta),
+  options,
+  );
+export const cacxVisitsDashboard = getAsyncLifecycle(() => import('./views/cacx-visits/cacx-visits-services.component'), {
+  featureName: 'cacx-visits',
+  moduleName,
+});
+
+export const cacxAppointmentsDashboardLink = getSyncLifecycle( createDashboardLink(caCxAppointmentsDashboardMeta),
+  options,
+);
+
+export const cacxAppointmentsDashboard = getAsyncLifecycle(() => import('./views/cacx-appointment/cacx-appointments.component'), {
+  featureName: 'cacx-appointments',
+  moduleName,
+},
+);
